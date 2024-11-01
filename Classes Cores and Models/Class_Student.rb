0cad337f5917c0ce20b289d6afffcc39d3c274ca
Effchan <134952909+Effchan@users.noreply.github.com>
2024-11-01 23:21:24 +0300

    Lab2.9 Add having contact info and git validations

--- Classes Cores and Models/Class_Student.rb
+++ Classes Cores and Models/Class_Student.rb
@@ -7,56 +7,66 @@ class Student
     end
   end
 
+  def has_contact_info?
+    return [@telephone, @telegram, @email].include?(@telephone) ||
+    [@telegram, @email].include?(@telegram) ||
+    [@telephone, @telegram, @email].include?(@email)
+  end
+
+  def has_git?
+    (@git != nil)
+  end
+
   def info
     "ID: #{@id}\nФИО: #{@first_name} #{@second_name} #{@third_name}\nКонтактная информация:\n\tТелефон: #{@telephone}\n\tTelegram: #{@telegram}\n\tEmail: #{@email}\n\tGit: #{@git}"
   end
 
   private
 
-  def is_valid_number?(value)
+  def self.is_valid_number?(value)
     /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/ === value
   end
 
-  def is_valid_username?(value)
+  def self.is_valid_username?(value)
     /^[a-zA-Z0-9_]+$/ === value
   end
 
-  def is_valid_email?(value)
+  def self.is_valid_email?(value)
     /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]{2,})\z/ === value
   end
 
-  def is_valid_url?(value)
+  def self.is_valid_url?(value)
     /^(https?:\/\/)?((github|bitbucket)\.org)(:[^\/\s]+)?(\/[^\s]+)$/ === value
   end
 
-  def is_valid_first_name?(value)
+  def self.is_valid_first_name?(value)
     /\A[A-ZА-я][a-zа-я]*\z/ === value
   end
 
-  def is_valid_middle_name?(value)
+  def self.is_valid_middle_name?(value)
     /\A[A-ZА-я][a-zа-я]*\z/ === value
   end
 
-  def is_valid_last_name?(value)
+  def self.is_valid_last_name?(value)
     /\A[A-ZА-я][a-zа-я]*\z/ === value
   end
 
   def valid_field?(field, value)
     case field
     when :telephone
-      is_valid_number?(value)
+      self.class.is_valid_number?(value)
     when :first_name
-      is_valid_first_name?(value)
+      self.class.is_valid_first_name?(value)
     when :second_name
-      is_valid_middle_name?(value)
+      self.class.is_valid_middle_name?(value)
     when :third_name
-      is_valid_last_name?(value)
+      self.class.is_valid_last_name?(value)
     when :telegram
-      is_valid_username?(value)
+      self.class.is_valid_username?(value)
     when :email
-      is_valid_email?(value)
+      self.class.is_valid_email?(value)
     when :git
-      is_valid_url?(value)
+      self.class.is_valid_url?(value)
     else
       true
     end
