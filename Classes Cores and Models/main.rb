7860782095b6af1dd47bd5704c64f65c45df8f5d
Effchan <134952909+Effchan@users.noreply.github.com>
2024-11-01 23:22:13 +0300

    Lab2.9 Check having contact info and git

--- Classes Cores and Models/main.rb
+++ Classes Cores and Models/main.rb
@@ -1,8 +1,11 @@
 require_relative 'C://Users//user//Desktop//s1.rb'
 
-student1 = Student.new(first_name: 'Олег', second_name: 'Олегович', third_name: 'Воевода', id: 1, telephone: '+7(918)123-45-67')
+student1 = Student.new(first_name: 'Олег', second_name: 'Олегович', third_name: 'Воевода', id: 1, telephone: '+7(918)123-45-67', git: 'http://github.org/VoeCode')
 student2 = Student.new(first_name: 'Александр', second_name: 'Павлович', third_name: 'Сыров', id: 2, email: 'cheeser211@gmail.com')
 
 puts student1.info
 puts "\n"
 puts student2.info
+
+puts "student #{student1.first_name()} #{student1.second_name()} #{student1.third_name()} has contact info: " + student1.has_contact_info?.to_s
+puts "student has git: " + student1.has_git?.to_s + ", student2 has git: " + student2.has_git?.to_s
