d2fec237d568a157cac5c3ffa307e72bdd268ff9
Effchan <134952909+Effchan@users.noreply.github.com>
2024-11-01 21:23:09 +0300

    Lab2.7 Add validation of telephone number field

--- Classes Cores and Models/Class_Student.rb
+++ Classes Cores and Models/Class_Student.rb
@@ -3,7 +3,18 @@ class Student
 
   def initialize(attributes = {})
     attributes.each do |key, value|
-      self.send("#{key}=", value) if respond_to?(key)
+      self.send("#{key}=", value) if respond_to?(key) && valid_field?(key, value)
+    end
+  end
+
+  private
+
+  def valid_field?(field, value)
+    case field
+    when :telephone
+      /\+7\(\d{3}\) \d{3}-\d{2}-\d{2}/ === value
+    else
+      true
     end
   end
 
