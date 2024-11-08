1178a500921b059e4e986561be2ac48cb02d14e3
Effchan <134952909+Effchan@users.noreply.github.com>
2024-11-09 00:24:04 +0300

    Lab2.2.5 changes in Student after adding superclasss

--- Classes Cores and Models/Class_Student.rb
+++ Classes Cores and Models/Class_Student.rb
@@ -1,14 +1,12 @@
-class Student
-  attr_reader :id, :first_name, :second_name, :third_name, :telephone, :telegram, :email, :git
-  attr_writer :id, :first_name, :second_name, :third_name
+require_relative 'C://Users//user//Desktop//Person.rb'
 
-  def initialize(attributes = {})
-    attributes.each do |key, value|
-      self.send("#{key}=", value) if respond_to?("#{key}=") && valid_field?(key, value)
-    end
+class Student < Person
+  attr_reader :telephone, :telegram, :email, :git
+
+  def info
+    "ID: #{@id}\nФИО: #{@first_name} #{@second_name} #{@third_name}\nКонтактная информация:\n\tТелефон: #{@telephone}\n\tTelegram: #{@telegram}\n\tEmail: #{@email}\nГит:\n\tGit: #{@git}"
   end
 
-  # Метод для установки контактных данных
   def set_contacts(contacts = {})
     contacts.each do |key, value|
       instance_variable_set("@#{key}", value) if [:telephone, :telegram, :email, :git].include?(key.to_sym)
@@ -21,26 +19,6 @@ class Student
 
   def has_git?
     !@git.nil?
-  end
-
-  def info
-    "ID: #{@id}\nФИО: #{@first_name} #{@second_name} #{@third_name}\nКонтактная информация:\n\tТелефон: #{@telephone}\n\tTelegram: #{@telegram}\n\tEmail: #{@email}\n\tGit: #{@git}"
-  end
-
-  def full_name_with_initials
-    "#{last_name} #{first_initial}.#{middle_initial}."
-  end
-
-  def first_initial
-    @first_name.chr
-  end
-
-  def middle_initial
-    @second_name.chr
-  end
-
-  def last_name
-    @third_name
   end
 
   def contact_info
@@ -58,54 +36,19 @@ class Student
     "#{full_name_with_initials}; #{contact_info}; #{git_info}"
   end
 
-  private
-
-  def self.is_valid_number?(value)
-    /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/ === value
+  def full_name_with_initials
+    "#{last_name} #{first_initial}.#{middle_initial}."
   end
 
-  def self.is_valid_username?(value)
-    /^[a-zA-Z0-9_]+$/ === value
+  def first_initial
+    @first_name.chr
   end
 
-  def self.is_valid_email?(value)
-    /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]{2,})\z/ === value
+  def middle_initial
+    @second_name.chr
   end
 
-  def self.is_valid_url?(value)
-    /^(https?:\/\/)?((github|bitbucket)\.org)(:[^\/\s]+)?(\/[^\s]+)$/ === value
-  end
-
-  def self.is_valid_first_name?(value)
-    /\A[A-ZА-я][a-zа-я]*\z/ === value
-  end
-
-  def self.is_valid_middle_name?(value)
-    /\A[A-ZА-я][a-zа-я]*\z/ === value
-  end
-
-  def self.is_valid_last_name?(value)
-    /\A[A-ZА-я][a-zа-я]*\z/ === value
-  end
-
-  def valid_field?(field, value)
-    case field
-    when :telephone
-      self.class.is_valid_number?(value)
-    when :first_name
-      self.class.is_valid_first_name?(value)
-    when :second_name
-      self.class.is_valid_middle_name?(value)
-    when :third_name
-      self.class.is_valid_last_name?(value)
-    when :telegram
-      self.class.is_valid_username?(value)
-    when :email
-      self.class.is_valid_email?(value)
-    when :git
-      self.class.is_valid_url?(value)
-    else
-      true
-    end
+  def last_name
+    @third_name
   end
 end
