3ae0a24ca2970a225e90b9bcdb452d088a882891
Effchan <134952909+Effchan@users.noreply.github.com>
2024-11-08 23:05:17 +0300

    Lab2.2.4 Add child class student_short

---
+++ Classes Cores and Models/Student_Short.rb
@@ -0,0 +1,20 @@
+require_relative 'C://Users//user//Desktop//s1.rb'
+
+class Student_short < Student
+  attr_reader :id, :name_and_initials, :git, :contact
+
+  def initialize(args)
+    @id = args[:id]
+    @name_and_initials = args[:name_and_initials]
+    @contact = args[:contact]
+    @git = args[:git]
+  end
+
+  def self.short_constructor(id, string)
+    data = string.split('; ')
+    new(id: id, name_and_initials: data[0], contact: data[1], git: data[2])
+  end
+
+  private_class_method :new
+  
+end
