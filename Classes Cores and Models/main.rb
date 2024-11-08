39c2f0076e9cf85494c0171c7b407ed9e93317fc
Effchan <134952909+Effchan@users.noreply.github.com>
2024-11-09 00:26:15 +0300

    Lab2.2.5 right realization for calling methods from classes after refactoring

--- Classes Cores and Models/main.rb
+++ Classes Cores and Models/main.rb
@@ -1,21 +1,26 @@
 require_relative 'C://Users//user//Desktop//s1.rb'
+require_relative 'C://Users//user//Desktop//StudentShort.rb'
+require_relative 'C://Users//user//Desktop//Person.rb'
 
-student1 = Student.new(first_name: 'Олег', second_name: 'Олегович', third_name: 'Воевода', id: 1, telephone: '+7(918)123-45-67', git: 'http://github.org/VoeCode')
-student2 = Student.new(first_name: 'Александр', second_name: 'Павлович', third_name: 'Сыров', id: 2, email: 'cheeser211@gmail.com')
+# Создаем объект класса Person
+person = Person.new(id: 1, first_name: 'Олег', second_name: 'Олегович', third_name: 'Воевода')
+puts "Информация о персоне:"
+puts "ID: #{person.id}, ФИО: #{person.first_name} #{person.second_name} #{person.third_name}"
 
-puts student1.info
-puts "\n"
-puts student2.info
+# Создаем объект класса Student
+student = Student.new(id: 2, first_name: 'Сергей', second_name: 'Петрович', third_name: 'Наскальный', telephone: '+7(900)111-22-33', email: 'petr0kozak@gmail.com', git: 'https://github.com/prost_petrovich')
+puts "\nИнформация о студенте:"
+puts student.info
+student.set_contacts(telegram: '@PetrP', git: 'https://github.com/ppetrov')
+puts student.info
+puts "Студент имеет контактную информацию: #{student.has_contact_info?}"
+puts "Студент имеет Git: #{student.has_git?}"
+puts "Контактная информация студента: #{student.contact_info}"
+puts "Git студента: #{student.git_info}"
+puts "Краткая информация о студенте: \n#{student.get_info}"
 
-student1.set_contacts({ telegram: '@oleg_voevoda', git: 'http://github.org/VoeCode'})
+# Создаем объект класса Student_short
+student_short = Student_short.short_constructor(3, "Сидоров С.С.; Тел.: +7(999)888-77-66; Git: https://github.com/SidorovSS")
+puts "\nИнформация о студенте из Student_short:"
+puts "ID: #{student_short.id}, Имя и инициалы: #{student_short.name_and_initials}, Контакт: #{student_short.contact}, Git: #{student_short.git}"
 
-puts student1.info
-
-puts "student #{student1.first_name()} #{student1.second_name()} #{student1.third_name()} has contact info: " + student1.has_contact_info?.to_s
-puts "student #{student1.first_name()} #{student1.second_name()} #{student1.third_name()} has git: " + student1.has_git?.to_s
-
-puts 'Контакты: ' + student1.contact_info
-
-puts 'Гитхаб: ' + student1.git_info
-
-puts "Общая информация кратко:\n" + student1.get_info
