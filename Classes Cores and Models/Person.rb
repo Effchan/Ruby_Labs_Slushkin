bfdb89f1abdb2377e0e4830a5f9659309adf314e
Effchan <134952909+Effchan@users.noreply.github.com>
2024-12-07 01:07:17 +0300

    Lab2.2.5 refactoring class and fix validate

--- Classes Cores and Models/Person.rb
+++ Classes Cores and Models/Person.rb
@@ -1,13 +1,27 @@
 class Person
-  attr_accessor :id, :first_name, :second_name, :third_name
+  attr_reader :id, :first_name, :second_name, :third_name
 
-  def initialize(attributes = {})
+  def initialize(attributes = {id:, first_name:, second_name:, third_name:})
     attributes.each do |key, value|
       send("#{key}=", value) if respond_to?("#{key}=") && valid_field?(key, value)
     end
   end
 
-  private
+  def id=(value)
+    @id = value if valid_field?(id, value)
+  end
+
+  def first_name=(value)
+    @first_name = value if valid_field?(first_name, value)
+  end
+
+  def second_name=(value)
+    @second_name = value if valid_field?(second_name, value)
+  end
+
+  def third_name=(value)
+    @third_name = value if valid_field?(third_name, value)
+  end
 
   def self.is_valid_first_name?(value)
     /\A[A-ZА-я][a-zа-я]*\z/ === value
@@ -21,24 +35,23 @@ class Person
     /\A[A-ZА-я][a-zа-я]*\z/ === value
   end
 
+  def self.is_valid_id(value)
+    value.is_a?(Integer)
+  end
+
   def valid_field?(field, value)
     case field
-    when :telephone
-      self.class.is_valid_number?(value)
+    when :id
+      self.class.is_valid_id(value)
     when :first_name
       self.class.is_valid_first_name?(value)
     when :second_name
       self.class.is_valid_middle_name?(value)
     when :third_name
       self.class.is_valid_last_name?(value)
-    when :telegram
-      self.class.is_valid_username?(value)
-    when :email
-      self.class.is_valid_email?(value)
-    when :git
-      self.class.is_valid_url?(value)
     else
       true
     end
   end
+  
 end
