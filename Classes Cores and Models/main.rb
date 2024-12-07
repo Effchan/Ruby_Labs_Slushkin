65559b2f5edda50968816619b67f9cdd906275cd
Effchan <134952909+Effchan@users.noreply.github.com>
2024-12-07 15:24:43 +0300

    Lab2.2.5 add demonstrating the work of initialize_from_student

--- Classes Cores and Models/main.rb
+++ Classes Cores and Models/main.rb
@@ -13,16 +13,19 @@ puts "\nИнформация о студенте:"
 puts student.info
 student.set_contacts(telegram: '@PetrP', git: 'https://github.com/ppetrov', email: 'petr0kozak@gmail.com')
 puts student.info
-puts "Прошел валидацию" if student.validate
+puts "Прошел валидацию" if student.validate?
 puts "Студент имеет контактную информацию: #{student.has_contact_info?}"
 puts "Студент имеет Git: #{student.has_git?}"
 puts "Контактная информация студента: #{student.contact_info}"
-puts "Git студента: #{student.git_info}"
+puts "Git студента: #{student.git}"
 puts "Краткая информация о студенте: \n#{student.get_info}"
 
 # Создаем объект класса Student_short
-student_short = Student_short.short_constructor(3, "Агарагаров С.С.; Тел.: +7(999)888-77-66; Git: https://github.com/SidorovSS")
+student_short = Student_short.initialize_from_string(3, "Агарагаров С.С.; Тел.: +7(999)888-77-66; Git: https://github.com/SidorovSS")
 puts "\nИнформация о студенте из Student_short:"
-puts "Имеет контакт или гит" if student_short.validate
 puts "ID: #{student_short.id}, Имя и инициалы: #{student_short.name_and_initials}, Контакт: #{student_short.contact}, #{student_short.git}"
-puts "Прошел валидацию" if student_short.short_validate
+puts "Прошел валидацию" if student_short.validate?
+puts "Инициализация short студента через объект родительского класса:"
+short_serega = Student_short.initialize_from_student(student)
+puts "ID: #{short_serega.id}, Имя и инициалы: #{short_serega.name_and_initials}, Контакт: #{short_serega.contact}, Гит: #{short_serega.git}"
+
